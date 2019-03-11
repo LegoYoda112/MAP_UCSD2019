b67f27ee318ee2f884f77e442a94d19635d2db92
Thomas G <thomas.godden@outlook.com>
2019-03-10 22:44:12 -0700

    Test downloaded 10 days worth of solrad data from Hanford

--- solradDownloader.py
+++ solradDownloader.py
@@ -13,6 +13,7 @@ try:
 except ftplib.all_errors, e:
     errorcode_string = str(e).split(None, 1)[0]
 
+#Setting the current working directory to 2019
 ftp.cwd('/data/radiation/solrad/hnx/2019/')
 
 directoryName = 'solrad_data'
@@ -22,6 +23,29 @@ if not os.path.exists(directoryName):
 # Move into the folder
 directoryPath = '%s/%s' % (os.getcwd(), directoryName)
 os.chdir(directoryPath)
+
+dateStart = 19001;
+dateEnd = 19010;
+
+currentDownloadDate = dateStart;
+
+print ftp.nlst()
+
+while currentDownloadDate <= dateEnd:
+
+    tempFileName = 'hnx%s.dat' % currentDownloadDate
+    file = open(tempFileName, 'wb')
+
+    print 'Downloading %s' % tempFileName
+
+    try:
+        ftp.retrbinary('RETR %s' % tempFileName, file.write)
+        print 'Successfully downloaded %s' % tempFileName
+    except ftplib.all_errors, e:
+        print 'Error downloading %s' % tempFileName
+        errorcode_string = str(e).split(None, 1)[0]
+
+    currentDownloadDate += 1
 
 ftp.close()
 print 'Closed'
