1b48e34272e2b98cb924c3e01b8ebe4114533150
LegoYoda112 <thomas.godden@outlook.com>
2019-05-09 21:43:25 -0700

    Updated prediction

--- data_analysis/generateModel.py
+++ data_analysis/generateModel.py
@@ -16,7 +16,7 @@ data = pd.read_csv("savedData.csv")
 #direct, temp, cloud_amount, wind_speed, humidity, probability_of_precipitation, clear_sky
 # data['probability_of_precipitation']
 
-unshapedX = np.array([data['temp'], data['cloud_amount'], data['humidity'], data['probability_of_precipitation'], data['clear_sky']])
+unshapedX = np.array([data['temp'], data['cloud_amount'],data['wind_speed'], data['humidity'], data['probability_of_precipitation'], data['clear_sky']])
 unshapedY = np.array([data["direct"], data["diffuse"]])
 #Inputs and outputs
 X = unshapedX.transpose(1,0)
@@ -26,19 +26,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0)
 
-model = MLPRegressor(max_iter = 200, verbose = True, hidden_layer_sizes=(500,100), solver = 'adam')
+model = MLPRegressor(max_iter = 200, verbose = True, hidden_layer_sizes=(50,10), solver = 'adam')
 
 model.fit(X_train, y_train)
 
 predictions = model.predict(X)
 
+print(np.sum(abs((y[:, 0]- predictions[:, 0])/y[:, 0]))/(len(y)))
+
 plt.plot(data.index, y[:, 0], label = 'Actual Diffuse')
 plt.plot(data.index, predictions[:, 0], label = 'Predicted Direct', linestyle='dashed', linewidth=1.5)
-plt.plot(data.index, y[:, 1], label = 'Actual Diffuse')
-plt.plot(data.index, predictions[:, 1], label = 'Predicted Diffuse', linestyle='dashed', linewidth=1.5)
-plt.plot(data.index, X[:,4])
+plt.plot(data.index, (predictions[:, 0] - y[:, 0]), label = "Error")
+#plt.plot(data.index, y[:, 1], label = 'Actual Diffuse')
+#plt.plot(data.index, predictions[:, 1], label = 'Predicted Diffuse', linestyle='dashed', linewidth=1.5)
+#plt.plot(data.index, X[:,4])
 
 plt.legend(loc = 'upper left')
 plt.show()
